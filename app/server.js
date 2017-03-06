a011164741c793ee286d8ed5f13925c7f2e16a46
Owen Garland <garland.owen@gmail.com>
2017-03-06 22:33:14 +0000

    Actually removed sockets.

--- app/server.js
+++ app/server.js
@@ -2,7 +2,6 @@ let express = require('express')
   , logger = require('morgan')
   , app = express()
   , addRoutes = require('./routes')
-  , attachSocket = require('./sockets')
   , port = process.env.PORT || 3000
   , path = require('path')
 
@@ -10,7 +9,6 @@ app.use(logger('dev'))
 app.use(express.static(path.join(__dirname, 'assets', 'build')))
 addRoutes(app)
 
-let server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`)
-  attachSocket(server)
 })
