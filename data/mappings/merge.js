5198d01c3e23dd53701064b0db6298bf70493707
Owen Garland <garland.owen@gmail.com>
2017-03-15 22:13:54 +0000

    Added mappings file

---
+++ data/mappings/merge.js
@@ -0,0 +1,11 @@
+const uniprot2refseq = require('./uniprot2refseq.json')
+    , cgd2uniprot = require('./cgd2uniprot.json')
+
+cgd2uniprot.map((id) => {
+  let refseq = uniprot2refseq.find(x => x.uniprot === id.Uniprot)
+  if (refseq) {
+    id.refseq = refseq.refseq
+    console.log(id)
+  }
+})
+
