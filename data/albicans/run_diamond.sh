f44abb5714d5994cd22164449e4799473f0ff184
Owen Garland <garland.owen@gmail.com>
2017-04-12 18:08:05 +0100

    Updated run diamond script

--- data/albicans/run_diamond.sh
+++ data/albicans/run_diamond.sh
@@ -1,7 +1,13 @@
 ./diamond makedb --in candida_albicans_proteins.fa -d albicans_ref -p 8
+
 ./diamond blastx -d albicans_ref.dmnd -q ../shehate/candida_shehate_codingseq.fa -o ../shehate/blastx_shehate_codingseq_albicans_proteins.xml -p 8 -f 5
-./diamond blastx -d albicans_ref.dmnd -q ../tropicalis/candida_tropicalis_codingseq.fa -o ../shehate/blastx_tropicalis_codingseq_albicans_proteins.xml -p 8 -f 5
+./diamond blastx -d albicans_ref.dmnd -q ../tropicalis/candida_tropicalis_codingseq.fa -o ../tropicalis/blastx_tropicalis_codingseq_albicans_proteins.xml -p 8 -f 5
 ./diamond blastx -d albicans_ref.dmnd -q ../boidinii/candida_boidinii_codingseq.fa -o ../boidinii/blastx_boidinii_codingseq_albicans_proteins.xml -p 8 -f 5
+
 xml2json ../shehate/blastx_shehate_codingseq_albicans_proteins.xml ../shehate/blastx_shehate_codingseq_albicans_proteins.json
-xml2json ../tropicalis/blastx_tropicalis_codingseq_albicans_proteins.xml ../shehate/blastx_tropicalis_codingseq_albicans_proteins.json
+xml2json ../tropicalis/blastx_tropicalis_codingseq_albicans_proteins.xml ../tropicalis/blastx_tropicalis_codingseq_albicans_proteins.json
 xml2json ../boidinii/blastx_boidinii_codingseq_albicans_proteins.xml ../boidinii/blastx_boidinii_codingseq_albicans_proteins.json
+
+rm ../shehate/blastx_shehate_codingseq_albicans_proteins.xml
+rm ../tropicalis/blastx_tropicalis_codingseq_albicans_proteins.xml
+rm ../boidinii/blastx_boidinii_codingseq_albicans_proteins.xml
