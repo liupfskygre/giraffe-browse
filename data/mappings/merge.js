8e6a795878655b2eda8ce623c21eef1d4dd1705d
Owen Garland <owen.garland@clock.co.uk>
2017-04-27 12:51:10 +0100

    Updated complete map with new uniprot ID's

--- data/mappings/merge.js
+++ data/mappings/merge.js
@@ -1,4 +1,3 @@
-// http://www.candidagenome.org/download/External_id_mappings/gp2protein.cgd.gz
 const cgd2uniprot = require(__dirname + '/cgd2uniprot.json')
     , cgdIds = require(__dirname + '/cgd_ids.json')
 
