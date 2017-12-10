9cc8c62c17a7f707f34ec6fd23c7a314da0ed5a9
Owen Garland <owen.garland@clock.co.uk>
2017-12-10 21:12:46 +0100

    Reverse complimented, needs to be checked

--- scripts/import.js
+++ scripts/import.js
@@ -14,14 +14,23 @@ createDatabase()
 HitModel.db.dropDatabase().then(() => {
   let contigs = fasta2json.ReadFasta(species.contigs)
 
-  gff2json.read(species.gff).on('data', (data) => {
-    let contig = contigs.find(x => x.head.split(' ')[0] === data.seqid)
+  gff2json.read(species.gff).on('data', (gff) => {
+    let contig = contigs.find(x => x.head.split(' ')[0] === gff.seqid)
 
     ContigModel.findOneAndUpdate({ head: contig.head }, contig, { upsert: true, new: true }).then((contigId) => {
       contigId = contigId._id
       // this needs to be checked, and probably reverse complimented
-      let cds = contig.seq.substr(data.start, data.end)
-        , hit = Object.assign({ species: species.name, contig: contigId, cds }, data)
+      let codingseq = contig.seq.substr(gff.start, gff.end)
+
+      if (gff.strand === '-') {
+        let reverse = codingseq.split('').reverse().join('')
+
+        codingseq = reverse.replace(/[ACTG]/g, (base) => {
+          return 'ACTG'.charAt('TGAC'.indexOf(base))
+        })
+      }
+
+      let hit = Object.assign({ species: species.name, contig: contigId, codingseq }, gff)
 
       HitModel.create(hit, (err) => {
         if (err) console.log('ERROR: ' + err)
