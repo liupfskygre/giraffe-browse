d204d5678ebbc0f036c46bb10f41170c0b56eb19
Owen Garland <owen.garland@clock.co.uk>
2018-01-23 11:27:29 +0000

    Fixed import to work properly

--- scripts/import.js
+++ scripts/import.js
@@ -11,87 +11,101 @@ const fasta2json = require('fasta2json')
     , HitModel = require('../app/models/hit')
     , ContigModel = require('../app/models/contig')
     , db = createDatabase()
+    , CONCURRENCY = 100 // bluebirdjs.com/docs/api/promise.map.html#map-option-concurrency
 
 db.dropDatabase().then(() => {
+  console.log('Database dropped')
   let contigs = fasta2json.ReadFasta(species.contigs)
 
   ContigModel.create(contigs).then(() => {
+    console.log('Contigs stored')
+
     let gffs = []
 
     gff2json.read(species.gff).on('data', (gff) => {
       gffs.push(gff)
     }).on('end', () => {
+      let saved = 0
+        , total = gffs.length
+
+      console.log(total + ' gff records found')
+
       Promise.map(gffs, (gff) => {
-        let contig = contigs.find(x => x.head.split(' ')[0] === gff.seqid)
+        return new Promise((resolve, reject) => {
+          let contig = contigs.find(x => x.head.split(' ')[0] === gff.seqid)
 
-        ContigModel.findOne({ head: contig.head }, { _id: true }).then((contigId) => {
+          ContigModel.findOne({ head: contig.head }, { _id: true }).then((contigId) => {
+            // {{{
+            let savedContig = { id: contigId._id, head: contig.head }
 
-          // {{{
-          let savedContig = { id: contigId._id, head: contig.head }
+            let codingseq = contig.seq.substring(gff.start - 1, gff.end)
 
-          let codingseq = contig.seq.substring(gff.start - 1, gff.end)
+            if (gff.strand === '-') {
+              let reverse = codingseq.split('').reverse().join('')
 
-          if (gff.strand === '-') {
-            let reverse = codingseq.split('').reverse().join('')
-
-            codingseq = reverse.replace(/[ACTG]/g, (base) => {
-              return 'ACTG'.charAt('TGAC'.indexOf(base))
-            })
-          }
-
-          let gffId = gff.attributes.ID
-          let product = gff.attributes.product
-          let locustag = gff.attributes.locus_tag
-          let name = gff.attributes.Name
-          let length = codingseq.length
-
-          delete gff.attributes.ID
-          delete gff.attributes.product
-          delete gff.attributes.locus_tag
-          delete gff.attributes.Name
-
-          let proteinseq = (new nt.Seq()).read(codingseq)
-          proteinseq = proteinseq.translate()
-
-          let hit = Object.assign(
-            { species: species.name
-            , gffId
-            , product
-            , locustag
-            , name
-            , length
-            , contig: savedContig
-            , codingseq
-            , proteinseq
-            }, gff)
-          // }}}
-
-          HitModel.create(hit).then(() => {}).catch(console.error)
-
-        }).catch(console.error)
-      }).then(() => {
-          db.collection('hits').createIndex(
-            { 'contig.head': 'text'
-            , gffId: 'text'
-            , locustag: 'text'
-            , product: 'text'
-            , name: 'text'
+              codingseq = reverse.replace(/[ACTG]/g, (base) => {
+                return 'ACTG'.charAt('TGAC'.indexOf(base))
+              })
             }
-          , { weights:
-              { product: 8
-              , 'contig.head': 6
-              , locustag: 10
-              , gffId: 10
-              , name: 10
-              }
-            , name: 'hit_search_index'
-            }
-          )
-          db.close(() => {
-            console.log('Finished.')
-            process.exit(0)
-          })
+
+            let gffId = gff.attributes.ID
+            let product = gff.attributes.product
+            let locustag = gff.attributes.locus_tag
+            let name = gff.attributes.Name
+            let length = codingseq.length
+
+            delete gff.attributes.ID
+            delete gff.attributes.product
+            delete gff.attributes.locus_tag
+            delete gff.attributes.Name
+
+            let proteinseq = (new nt.Seq()).read(codingseq)
+            proteinseq = proteinseq.translate()
+
+            let hit = Object.assign(
+              { species: species.name
+              , gffId
+              , product
+              , locustag
+              , name
+              , length
+              , contig: savedContig
+              , codingseq
+              , proteinseq
+              }, gff)
+            // }}}
+            HitModel.create(hit).then(() => {
+              saved++
+              if (saved % 1000 === 0) console.log(saved + ' saved')
+              resolve()
+            }).catch(reject)
+          }).catch(reject)
         })
+      }, {concurrency: CONCURRENCY}).then(() => {
+        console.log('Creating indexes')
+
+        db.collection('hits').createIndex(
+          { 'contig.head': 'text'
+          , gffId: 'text'
+          , locustag: 'text'
+          , product: 'text'
+          , name: 'text'
+          }
+        , { weights:
+            { product: 8
+            , 'contig.head': 6
+            , locustag: 10
+            , gffId: 10
+            , name: 10
+            }
+          , name: 'hit_search_index'
+          }
+        )
+        db.close(() => {
+          console.log('Finished.')
+          process.exit(0)
+        })
+      }).catch((err) => { console.log(err) })
     })
-  }).catch(console.err)
+  }).catch((err) => console.log(err))
 })
